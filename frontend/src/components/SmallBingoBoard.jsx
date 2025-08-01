cc58e4d985ffed092de904706e47301b0f9b7586
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-07-31 21:46:50 -0400

    started working on collapsible bingo boards

---
+++ frontend/src/components/SmallBingoBoard.jsx
@@ -0,0 +1,51 @@
+import "../css/componentcss/SmallBoard.css"
+import { useState } from "react"
+import { BingoBoard } from "./BingoBoard";
+
+export function SmallBingoBoard({team}){
+    // start with open state being set to false
+    const [isOpen, setIsOpen] = useState(false);
+
+    const numRows = 7;
+    const numCols = 7;
+    let tileCount = 0;
+    const tiles = [];
+
+    for (let i = 1; i <= numRows; i++) {
+        for (let j = 1; j <= numCols; j++) {
+            tileCount++
+            const tileId = `tile${(i - 1) * numCols + j}`;
+            tiles.push(
+                <div
+                    className={isOpen ? "big-tiles" : "small-tiles"}
+                    id={tileId}
+                    key= {tileCount}
+                    data-x={i}
+                    data-y={j}
+                >
+                    {tileCount}
+                </div>
+            );
+        }
+    }
+
+    return (
+        <>
+        <div className="small-board-page">
+            <div className="team-title">This is {team} name</div>
+            <div 
+                className={isOpen ? "big-bingo-board" : "small-bingo-board"}
+                onClick={() => setIsOpen((prev) => !prev)}
+                >
+
+                {isOpen ? 'Collapse' : 'Expand'} 
+                <div 
+                    className={isOpen ? "big-tiles-container" : "small-tiles-container"}
+                >
+                    {tiles}
+                </div>
+            </div> 
+        </div>
+        </>
+    )
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { BingoBoard } from "../components/BingoBoard";
+import { SmallBingoBoard } from "../components/SmallBingoBoard";
 import { CollapsibleSection } from "../components/util/Collapsible";
 import "../css/HomePage.css"
 import { useState } from "react";
@@ -6,28 +7,28 @@ import { useState } from "react";
 export function HomePage(){
     //placeholder variables to call each team from the backend
     //will probably end up creating a map to call a dynamic number of teams but this works for now xd
-    const team1=""
-    const team2=""
-    const team3=""
-    const team4=""
-    const team5=""
-    const team6=""
+    const team1="Team1"
+    const team2="Team2"
+    const team3="Team3"
+    const team4="Team4"
+    const team5="Team5"
+    const team6="Team6"
 
     return (
         <div className="home-page">
             <h3>EVIL DAVE'S TOTALLY EVIL BINGO EVENT</h3>
-            <div className="collapsible-buttons">
+            {/* <div className="collapsible-buttons">
                 <CollapsibleSection label="Button 1">
                     <p>Button 1 text.</p>
                 </CollapsibleSection>
-            </div>
+            </div> */}
             <div className="bingo-container">
-                <BingoBoard team={team1}/>
-                <BingoBoard team={team2}/>
-                <BingoBoard team={team3}/>
-                <BingoBoard team={team4}/>
-                <BingoBoard team={team5}/>
-                <BingoBoard team={team6}/>
+                <SmallBingoBoard team={team1} />
+                <SmallBingoBoard team={team2} />
+                <SmallBingoBoard team={team3} />
+                <SmallBingoBoard team={team4} />
+                <SmallBingoBoard team={team5} />
+                <SmallBingoBoard team={team6} />
             </div>
         </div>
     );
