44a7a5dab771e8b7645df66963f10ba8748a3e9e
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-08-05 17:34:27 -0400

    css styling fixes

--- frontend/src/components/util/NavBar.jsx
+++ frontend/src/components/util/NavBar.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 import "../../css/componentcss/NavBar.css"
 
 export function NavBar(){
-    return <nav className="navbar">
-        <div className="navBarContainer">
-            <Link to="/" className="nav-link">Home</Link>
+    return (
+        <div className="navbar-component"> 
+            <nav className="navbar">
+                <div className="navBar-container">
+                    <Link to="/" className="nav-link">Home</Link>
+                </div>
+                <div className="navBar-links">
+                    <Link to="/teampage" className="nav-link">Team</Link>
+                    <Link to="/adminpage" className="nav-link">Admin</Link>
+                </div>
+            </nav>
         </div>
-        <div className="navBarLinks">
-            <Link to="/teampage" className="nav-link">Team</Link>
-            <Link to="/adminpage" className="nav-link">Admin</Link>
-        </div>
-    </nav>
+    )
 }
