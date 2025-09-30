cd11f3772126f339504a58438af2b11229a9a4dd
Emil Rosin <SaltisRS>
2025-09-30 04:23:10 +0200

    Rename variables

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,16 +3,18 @@ import os
 from dotenv import load_dotenv
 from loguru import logger
 from fastapi import FastAPI
-from .routers.admin import router as adr
-from .routers.auth import router as ahr
-from .routers.team import router as ter
+from .routers.admin import router as admin
+from .routers.auth import router as auth
+from .routers.team import router as team
+from .routers.newsfeed import router as newsfeed
 
 
 load_dotenv()
 app = FastAPI(debug=os.getenv("BACKEND_DEBUG", False))
-app.include_router(adr)
-app.include_router(ahr)
-app.include_router(ter)
+app.include_router(admin)
+app.include_router(auth)
+app.include_router(team)
+app.include_router(newsfeed)
 
 
 @app.route("/", ["GET"])
