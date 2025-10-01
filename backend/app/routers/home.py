07859c01620ba60a17f5c70cbc049cb181c755d8
Emil Rosin <SaltisRS>
2025-10-02 00:40:06 +0200

    Add frontpage

---
+++ backend/app/routers/home.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Request, HTTPException, status, Depends
+
+from loguru import logger
+
+from ..models.bingo import Frontpage, Team
+from ..database.mongoclient import team_collection
+
+
+router = APIRouter(prefix="/home")
+tc = team_collection()
+
+
+
+async def get_all_teams():
+    cursor = tc.find({})
+    all_teams: list[Team] = []
+    
+    for team in cursor:
+        team["phrase"] = "Thurgo Is Displeased"
+        all_teams.append(Team.model_validate(team))
+
+    return Frontpage.model_validate(all_teams)
+        
+
+
+
+
+
+@router.get("/", response_model=Frontpage)
+async def index(teams: Frontpage = Depends(get_all_teams)):
+    """
+    Retrieves a list of all teams to be used for the frontpage of EvilDaveBingo.com
+    """
+    return teams
